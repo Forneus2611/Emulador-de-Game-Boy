71dd9773e60df56a93eb1fcafdb17a260f4b1b1d
Ivan <juancarlo.0s012@gmail.com>
2025-06-12 01:53:24 -0400

    Niveles 1-5 y antirrebote implementado

--- Codigo/Larry's/Larry.ino/Larry.ino.ino
+++ Codigo/Larry's/Larry.ino/Larry.ino.ino
@@ -16,11 +16,49 @@ Adafruit_ST7735 tft = Adafruit_ST7735(&mySPI, TFT_CS, TFT_DC, TFT_RST);
 #define BTN_LEFT   4
 #define BTN_RIGHT  5
 #define BTN_START  7
+#define BTN_BACK   6
 
 const int size = 10;
 const int step = 10;
+const int MAX_CAJAS = 5;
 
-const char nivel[13][16] = {
+int playerX, playerY;
+int boxX[MAX_CAJAS], boxY[MAX_CAJAS];
+int goalX[MAX_CAJAS], goalY[MAX_CAJAS];
+int cajasEncontradas = 0;
+int metasEncontradas = 0;
+bool victory = false;
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 150;
+
+// Sprites de Larry y Caja
+static const uint32_t larry_data[1][100] = {{
+  0xff000000, 0xffffff00, 0xffffff00, 0xffffff00, 0xff000000, 0xff000000, 0xffffff00, 0xffffff00, 0xffffff00, 0xff000000,
+  0xff000000, 0xffffff00, 0xff000000, 0xffffff00, 0xff000000, 0xff000000, 0xffffff00, 0xff000000, 0xffffff00, 0xff000000,
+  0xff000000, 0xffffffff, 0xffffff00, 0xffffff00, 0xff000000, 0xff000000, 0xffffffff, 0xffffff00, 0xffffff00, 0xff000000,
+  0xff000000, 0xff000000, 0xffffff00, 0xff000000, 0xff000000, 0xff000000, 0xff000000, 0xffffff00, 0xff000000, 0xff000000,
+  0xff000000, 0xff000000, 0xffffff00, 0xff000000, 0xff000000, 0xff000000, 0xff000000, 0xffffff00, 0xff000000, 0xff000000,
+  0xff000000, 0xff000000, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00,
+  0xff000000, 0xff000000, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00,
+  0xff000000, 0xffffff00, 0xffffff00, 0xff000000, 0xff000000, 0xff000000, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00,
+  0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00,
+  0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00, 0xffffff00
+}};
+
+static const uint32_t boxSprite[1][100] = {{
+  0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513,
+  0xff8b4513, 0xff000000, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xff000000, 0xff8b4513,
+  0xff8b4513, 0xffa0522d, 0xff000000, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xff000000, 0xffa0522d, 0xff8b4513,
+  0xff8b4513, 0xffa0522d, 0xffa0522d, 0xff000000, 0xffa0522d, 0xffa0522d, 0xff000000, 0xffa0522d, 0xffa0522d, 0xff8b4513,
+  0xff8b4513, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xff000000, 0xff000000, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xff8b4513,
+  0xff8b4513, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xff000000, 0xff000000, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xff8b4513,
+  0xff8b4513, 0xffa0522d, 0xffa0522d, 0xff000000, 0xffa0522d, 0xffa0522d, 0xff000000, 0xffa0522d, 0xffa0522d, 0xff8b4513,
+  0xff8b4513, 0xffa0522d, 0xff000000, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xff000000, 0xffa0522d, 0xff8b4513,
+  0xff8b4513, 0xff000000, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xffa0522d, 0xff000000, 0xff8b4513,
+  0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513, 0xff8b4513
+}};
+// Niveles
+const char nivel1[13][16] = {
   {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
   {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
   {'1','1','1','1','1','1','1','2','2','1','1','1','1','1','1','1'},
@@ -36,30 +74,183 @@ const char nivel[13][16] = {
   {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'}
 };
 
-int playerX, playerY;
-int box1X, box1Y;
-int box2X, box2Y;
-int goal1X, goal1Y;
-int goal2X, goal2Y;
-bool victory = false;
+const char nivel2[13][16] = {
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','2','2','2','2','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','2','2','2','M','M','2','1','1','1','1','1'},
+  {'1','1','1','1','1','2','C','C','C','L','2','1','1','1','1','1'},
+  {'1','1','1','1','1','2','2','1','2','M','2','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'}
+};
 
-void cargarEntidades() {
-  int cajasEncontradas = 0;
-  int metasEncontradas = 0;
-  for (int y = 0; y < 13; y++) {
-    for (int x = 0; x < 16; x++) {
-      char ch = nivel[y][x];
-      if (ch == 'L') { playerX = x * size; playerY = y * size; }
-      if (ch == 'C') {
-        if (cajasEncontradas == 0) { box1X = x * size; box1Y = y * size; cajasEncontradas++; }
-        else { box2X = x * size; box2Y = y * size; }
-      }
-      if (ch == 'M') {
-        if (metasEncontradas == 0) { goal1X = x * size; goal1Y = y * size; metasEncontradas++; }
-        else { goal2X = x * size; goal2Y = y * size; }
+const char nivel3[13][16] = {
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','L','M','C','2','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','2','C','1','2','2','1','1','1','1','1','1'},
+  {'1','1','1','1','1','2','C','2','M','2','1','1','1','1','1','1'},
+  {'1','1','1','1','1','M','2','2','2','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'}
+};
+
+const char nivel4[13][16] = {
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','2','2','2','2','1','1','1','1','1','1'},
+  {'1','1','1','1','1','2','M','M','2','2','2','1','1','1','1','1'},
+  {'1','1','1','1','1','2','1','2','C','C','C','1','1','1','1','1'},
+  {'1','1','1','1','1','2','2','2','1','L','M','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'}
+};
+
+const char nivel5[13][16] = {
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','2','2','M','2','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','2','1','M','2','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','2','2','M','2','2','1','1','1','1','1'},
+  {'1','1','1','1','1','1','2','C','C','C','2','1','1','1','1','1'},
+  {'1','1','1','1','1','1','2','2','1','2','L','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
+  {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'}
+};
+
+
+// Array de niveles
+const char (*niveles[])[16] = {nivel1, nivel2, nivel3, nivel4, nivel5};
+const int TOTAL_NIVELES = sizeof(niveles) / sizeof(niveles[0]);
+int nivelActual = 0;
+const char (*nivel)[16] = niveles[nivelActual];
+
+// Funciones
+void drawLarry(int x, int y) {
+  for (int r = 0; r < size; r++) {
+    for (int c = 0; c < size; c++) {
+      uint32_t color32 = larry_data[0][r * size + c];
+      if ((color32 & 0xFFFFFF) != 0x000000) {
+        uint16_t color16 = tft.color565((color32 >> 16) & 0xFF, (color32 >> 8) & 0xFF, color32 & 0xFF);
+        tft.drawPixel(x + c, y + r, color16);
       }
     }
   }
+}
+
+void drawBox(int x, int y) {
+  for (int r = 0; r < size; r++) {
+    for (int c = 0; c < size; c++) {
+      uint32_t color32 = boxSprite[0][r * size + c];
+      if ((color32 & 0xFFFFFF) != 0x000000) {
+        uint16_t color16 = tft.color565((color32 >> 16) & 0xFF, (color32 >> 8) & 0xFF, color32 & 0xFF);
+        tft.drawPixel(x + c, y + r, color16);
+      }
+    }
+  }
+}
+
+void clearSprite(int x, int y) {
+  int cx = x / size;
+  int cy = y / size;
+  char ch = nivel[cy][cx];
+  if (ch == '1') tft.fillRect(x, y, size, size, tft.color565(100, 100, 100));
+  else if (ch == 'M') tft.fillRect(x, y, size, size, ST77XX_GREEN);
+  else tft.fillRect(x, y, size, size, ST77XX_BLACK);
+}
+
+bool isWall(int x, int y) {
+  int cx = x / size;
+  int cy = y / size;
+  return nivel[cy][cx] == '1';
+}
+
+bool isInside(int x, int y) {
+  return x >= 0 && x <= tft.width() - size && y >= 0 && y <= tft.height() - size;
+}
+
+bool isColliding(int ax, int ay, int bx, int by) {
+  int margin = 4;
+  return abs(ax - bx) < size - margin && abs(ay - by) < size - margin;
+}
+
+void drawScene() {
+  for (int i = 0; i < metasEncontradas; i++) {
+    bool cajaEnMeta = false;
+    for (int j = 0; j < cajasEncontradas; j++) {
+      if (boxX[j] == goalX[i] && boxY[j] == goalY[i]) {
+        cajaEnMeta = true;
+        break;
+      }
+    }
+    tft.fillRect(goalX[i], goalY[i], size, size, cajaEnMeta ? ST77XX_BLUE : ST77XX_GREEN);
+  }
+
+  for (int i = 0; i < cajasEncontradas; i++) {
+    drawBox(boxX[i], boxY[i]);
+  }
+  drawLarry(playerX, playerY);
+}
+
+void pantallaNivel(int numeroNivel) {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(2);
+  tft.setCursor(30, 40);
+  tft.print("Nivel ");
+  tft.println(numeroNivel);
+  delay(1500);
+}
+
+void setupNivel() {
+  cajasEncontradas = 0;
+  metasEncontradas = 0;
+  
+  tft.fillScreen(ST77XX_BLACK);
+  
+  for (int y = 0; y < 13; y++) {
+    for (int x = 0; x < 16; x++) {
+      int px = x * size;
+      int py = y * size;
+      char ch = nivel[y][x];
+      if (ch == '1') tft.fillRect(px, py, size, size, tft.color565(100, 100, 100));
+      else if (ch == 'M') {
+        tft.fillRect(px, py, size, size, ST77XX_GREEN);
+        goalX[metasEncontradas] = px;
+        goalY[metasEncontradas] = py;
+        metasEncontradas++;
+      }
+      else if (ch == 'L') {
+        playerX = px; playerY = py;
+      }
+      else if (ch == 'C') {
+        boxX[cajasEncontradas] = px;
+        boxY[cajasEncontradas] = py;
+        cajasEncontradas++;
+      }
+    }
+  }
+  drawScene();
 }
 
 void setup() {
@@ -73,36 +264,27 @@ void setup() {
   pinMode(BTN_RIGHT, INPUT_PULLUP);
   pinMode(BTN_START, INPUT_PULLUP);
 
-  tft.fillScreen(ST77XX_BLACK);
-  cargarEntidades();
-
-  for (int y = 0; y < 13; y++) {
-    for (int x = 0; x < 16; x++) {
-      int px = x * size;
-      int py = y * size;
-      char ch = nivel[y][x];
-      if (ch == '1') tft.fillRect(px, py, size, size, ST77XX_WHITE);
-      else if (ch == 'M') tft.fillRect(px, py, size, size, ST77XX_GREEN);
-      else if (ch == '2') tft.fillRect(px, py, size, size, ST77XX_BLACK);
-    }
-  }
-
-  drawScene();
+  pantallaNivel(nivelActual + 1);
+  setupNivel();
 }
 
 void loop() {
   if (victory) return;
-  int dx = 0, dy = 0;
-  if (digitalRead(BTN_UP) == LOW) dy = -step;
-  if (digitalRead(BTN_DOWN) == LOW) dy = step;
-  if (digitalRead(BTN_LEFT) == LOW) dx = -step;
-  if (digitalRead(BTN_RIGHT) == LOW) dx = step;
 
-  if (digitalRead(BTN_START) == LOW) {
-    victory = false;
-    setup();
-    delay(300);
-    return;
+  int dx = 0, dy = 0;
+  unsigned long currentTime = millis();
+
+  if (currentTime - lastDebounceTime > debounceDelay) {
+    if (digitalRead(BTN_UP) == LOW) { dy = -step; lastDebounceTime = currentTime; }
+    else if (digitalRead(BTN_DOWN) == LOW) { dy = step; lastDebounceTime = currentTime; }
+    else if (digitalRead(BTN_LEFT) == LOW) { dx = -step; lastDebounceTime = currentTime; }
+    else if (digitalRead(BTN_RIGHT) == LOW) { dx = step; lastDebounceTime = currentTime; }
+    else if (digitalRead(BTN_START) == LOW) {
+      victory = false;
+      setupNivel();
+      lastDebounceTime = currentTime;
+      return;
+    }
   }
 
   if (dx != 0 || dy != 0) {
@@ -111,83 +293,79 @@ void loop() {
 
     if (!isInside(newPlayerX, newPlayerY) || isWall(newPlayerX, newPlayerY)) return;
 
-    bool empujaCaja1 = isColliding(newPlayerX, newPlayerY, box1X, box1Y);
-    bool empujaCaja2 = isColliding(newPlayerX, newPlayerY, box2X, box2Y);
+    bool empuja = false;
+    for (int i = 0; i < cajasEncontradas; i++) {
+      if (isColliding(newPlayerX, newPlayerY, boxX[i], boxY[i])) {
+        int newBoxX = boxX[i] + dx;
+        int newBoxY = boxY[i] + dy;
 
-    if (empujaCaja1 || empujaCaja2) {
-      int& bX = empujaCaja1 ? box1X : box2X;
-      int& bY = empujaCaja1 ? box1Y : box2Y;
-      int newBoxX = bX + dx;
-      int newBoxY = bY + dy;
-      if (isInside(newBoxX, newBoxY) && !isWall(newBoxX, newBoxY) &&
-          !isColliding(newBoxX, newBoxY, empujaCaja1 ? box2X : box1X, empujaCaja1 ? box2Y : box1Y)) {
-        clearSprite(bX, bY);
-        bX = newBoxX;
-        bY = newBoxY;
-        clearSprite(playerX, playerY);
-        playerX = newPlayerX;
-        playerY = newPlayerY;
-        drawScene();
+        bool colisionOtraCaja = false;
+        for (int j = 0; j < cajasEncontradas; j++) {
+          if (j != i && isColliding(newBoxX, newBoxY, boxX[j], boxY[j])) {
+            colisionOtraCaja = true;
+            break;
+          }
+        }
+
+        if (isInside(newBoxX, newBoxY) && !isWall(newBoxX, newBoxY) && !colisionOtraCaja) {
+          clearSprite(boxX[i], boxY[i]);
+          boxX[i] = newBoxX;
+          boxY[i] = newBoxY;
+          clearSprite(playerX, playerY);
+          playerX = newPlayerX;
+          playerY = newPlayerY;
+          drawScene();
+          empuja = true;
+          break;
+        } else {
+          return; // No puede empujar
+        }
       }
-    } else {
+    }
+
+    if (!empuja) {
       clearSprite(playerX, playerY);
       playerX = newPlayerX;
       playerY = newPlayerY;
       drawScene();
     }
 
-    if (((box1X == goal1X && box1Y == goal1Y) && (box2X == goal2X && box2Y == goal2Y)) ||
-        ((box1X == goal2X && box1Y == goal2Y) && (box2X == goal1X && box2Y == goal1Y))) {
+    bool victoriaActual = true;
+    for (int i = 0; i < metasEncontradas; i++) {
+      bool metaOcupada = false;
+      for (int j = 0; j < cajasEncontradas; j++) {
+        if (boxX[j] == goalX[i] && boxY[j] == goalY[i]) {
+          metaOcupada = true;
+          break;
+        }
+      }
+      if (!metaOcupada) {
+        victoriaActual = false;
+        break;
+      }
+    }
+
+    if (victoriaActual) {
       tft.setTextColor(ST77XX_WHITE);
       tft.setTextSize(2);
       tft.setCursor(20, 80);
       tft.println("VICTORIA!");
-      victory = true;
+      delay(1000);
+
+      nivelActual++;
+      if (nivelActual < TOTAL_NIVELES) {
+        nivel = niveles[nivelActual];
+        victory = false;
+        pantallaNivel(nivelActual + 1);
+        setupNivel();
+      } else {
+        tft.fillScreen(ST77XX_BLACK);
+        tft.setCursor(10, 40);
+        tft.setTextSize(2);
+        tft.setTextColor(ST77XX_GREEN);
+        tft.println("JUEGO COMPLETADO!");
+        victory = true;
+      }
     }
-
-    delay(100);
   }
-}
-
-void drawScene() {
-  bool cajaEnMeta1 = (box1X == goal1X && box1Y == goal1Y) || (box2X == goal1X && box2Y == goal1Y);
-  bool cajaEnMeta2 = (box1X == goal2X && box1Y == goal2Y) || (box2X == goal2X && box2Y == goal2Y);
-  tft.fillRect(goal1X, goal1Y, size, size, cajaEnMeta1 ? ST77XX_BLUE : ST77XX_GREEN);
-  tft.fillRect(goal2X, goal2Y, size, size, cajaEnMeta2 ? ST77XX_BLUE : ST77XX_GREEN);
-
-  drawBox(box1X, box1Y);
-  drawBox(box2X, box2Y);
-  drawLarry(playerX, playerY);
-}
-
-void drawLarry(int x, int y) {
-  tft.fillRect(x, y, size, size, ST77XX_RED);
-}
-
-void drawBox(int x, int y) {
-  tft.fillRect(x, y, size, size, ST77XX_YELLOW);
-}
-
-void clearSprite(int x, int y) {
-  int cx = x / size;
-  int cy = y / size;
-  char ch = nivel[cy][cx];
-  if (ch == '1') tft.fillRect(x, y, size, size, ST77XX_WHITE);
-  else if (ch == 'M') tft.fillRect(x, y, size, size, ST77XX_GREEN);
-  else tft.fillRect(x, y, size, size, ST77XX_BLACK);
-}
-
-bool isInside(int x, int y) {
-  return x >= 0 && x <= tft.width() - size && y >= 0 && y <= tft.height() - size;
-}
-
-bool isColliding(int ax, int ay, int bx, int by) {
-  int margin = 4;
-  return abs(ax - bx) < size - margin && abs(ay - by) < size - margin;
-}
-
-bool isWall(int x, int y) {
-  int cx = x / size;
-  int cy = y / size;
-  return nivel[cy][cx] == '1';
 }
